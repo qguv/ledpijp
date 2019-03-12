2452b4ed24f46684403bc86646837a10daa4cf8e
Quint Guvernator <quint@guvernator.net>
2019-03-13 00:23:09 +0100

    minimal web example!

--- src/espijp.ino
+++ src/espijp.ino
@@ -1,8 +1,11 @@
 #include <ESP8266WiFi.h>
+#include <math.h>
 #include <Arduino.h>
 
 #define PORT 80
 #define NUM_LEDS 71
+#define DATA_PIN 13
+#define CLK_PIN 14
 
 const char *ssid = "comet compat";
 const char *password = "logicalis";
@@ -36,34 +39,45 @@ WiFiServer server(PORT);
 // respond to 'GET /'
 void respond_index(WiFiClient client)
 {
-	client.print(F("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!doctype html><html><body><h1>"));
+	client.print(F(
+		"HTTP/1.1 200 OK\r\n"
+		"Content-Type: text/html\r\n"
+		"\r\n"
+		"<!doctype html>\n"
+		"<html>\n"
+		"\t<head>\n"
+		"\t\t<meta charset='utf-8' />\n"
+		"\t\t<title>espijp</title>\n"
+		"\t\t<script>\n"
+		"\t\t\tfunction set_led(val) {\n"
+		"\t\t\t\tfetch('/' + val).then(res => {\n"
+		"\t\t\t\t\tif (res.ok)\n"
+		"\t\t\t\t\t\tres.text().then(body => document.querySelector('h1').innerHTML = body);\n"
+		"\t\t\t\t});\n"
+		"\t\t\t}\n"
+		"\t\t</script>\n"
+		"\t</head>\n"
+		"\t<body>\n"
+		"\t\t<h1>\n"
+	));
 	client.print(animation_names[(int) anim]);
-	client.print(F(" ("));
-	client.print((int) anim);
-	client.print(F(")"));
-	client.print(F("</h1><ul>"));
+	client.print(F("</h1>\n\t\t<ul>"));
 
 	for (int i = 0; i < (int) ANIM_UNDEFINED; i++) {
-		if (i == (int) anim) {
-			client.print(F("<li style='font-style: italic;'>"));
-			client.print(animation_names[i]);
-			client.print(F("</li>"));
-		} else {
-			client.print(F("<li><a href='http://"));
-			client.print(WiFi.localIP());
-			client.print(F("/"));
-			client.print(animation_names[i]);
-			client.print(F("'>"));
-			client.print(animation_names[i]);
-			client.print(F("</a></li>"));
-		}
+		client.print(F("\n\t\t\t<li><a href='#' onclick='set_led(\""));
+		client.print(animation_names[i]);
+		client.print(F("\");'>"));
+		client.print(animation_names[i]);
+		client.print(F("</a></li>"));
 	}
-	client.print(F("</ul></body></html>"));
+	client.print(F("\n\t\t\t<li><a href='#' onclick='set_led(\"next\")'>next</a></li>"));
+	client.print(F("\n\t\t</ul>\n\t</body>\n</html>"));
 }
 
-void respond_ok(WiFiClient client)
+void respond_new_anim(WiFiClient client)
 {
-	client.print(F("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nOK"));
+	client.print(F("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n"));
+	client.print(animation_names[new_anim]);
 }
 
 void respond_not_found(WiFiClient client)
@@ -74,6 +88,9 @@ void respond_not_found(WiFiClient client)
 // get desired animation from request
 enum animation get_animation(String request)
 {
+	if (request.startsWith("GET /next "))
+		return (enum animation) (((int) anim + 1) % (int) ANIM_UNDEFINED);
+
 	for (int i = 0; i < (int) ANIM_UNDEFINED; i++)
 		if (request.startsWith(String("GET /") + String(animation_names[i]) + String(" ")))
 			return (enum animation) i;
@@ -91,14 +108,63 @@ void append_led(unsigned char r, unsigned char g, unsigned char b)
 	cbuf_pos %= NUM_LEDS;
 }
 
+void apa102_header()
+{
+	for (int _ = 0; _ < 32; _++) {
+		digitalWrite(DATA_PIN, 0);
+		digitalWrite(CLK_PIN, 1);
+		digitalWrite(DATA_PIN, 0);
+		digitalWrite(CLK_PIN, 0);
+	}
+}
+
+void apa102_footer()
+{
+	for (int _ = 0; _ < (NUM_LEDS / 2); _++) {
+		digitalWrite(DATA_PIN, 0);
+		digitalWrite(CLK_PIN, 1);
+		digitalWrite(DATA_PIN, 0);
+		digitalWrite(CLK_PIN, 0);
+	}
+}
+
+void apa102_frame(unsigned char red, unsigned char green, unsigned char blue)
+{
+	// construct it backwards to save a few cycles
+	unsigned int color = 0;
+	color |= red;
+	color <<= 8;
+	color |= green;
+	color <<= 8;
+	color |= blue;
+	color <<= 8;
+	color |= 0xff;
+
+	for (int b = 0; b < 32; b++, color >>= 1) {
+		int bit = color & 1;
+		digitalWrite(DATA_PIN, bit);
+		digitalWrite(CLK_PIN, 1);
+		digitalWrite(DATA_PIN, bit);
+		digitalWrite(CLK_PIN, 0);
+	}
+}
+
 void blit_cbuf_leds()
 {
-	// TODO blit
+	apa102_header();
+	for (int i = 0; i < NUM_LEDS; i++) {
+		int red_i = ((cbuf_pos + i) % NUM_LEDS) * 3;
+		apa102_frame(cbuf[red_i], cbuf[red_i + 1], cbuf[red_i + 2]);
+	}
+	apa102_footer();
 }
 
 void blit_solid_leds(unsigned char r, unsigned char g, unsigned char b)
 {
-	// TODO blit
+	apa102_header();
+	for (int i = 0; i < NUM_LEDS; i++)
+		apa102_frame(r, g, b);
+	apa102_footer();
 }
 
 /* hue [0, 360), sat [0, 1], val [0, 1], rgb [0, 255] */
@@ -183,6 +249,7 @@ void cycle_anim()
 	case ANIM_RAINBOW:
 		hsv2rgb(frame, 1, 1, &r, &g, &b);
 		append_led(r, g, b);
+		blit_cbuf_leds();
 		break;
 
 	case ANIM_CYCLE:
@@ -199,31 +266,25 @@ void cycle_anim()
 
 void setup()
 {
-	Serial.begin(115200);
-	Serial.println();
-
 	anim = new_anim = ANIM_RAINBOW;
 
-	pinMode(LED_BUILTIN, OUTPUT);
-	digitalWrite(LED_BUILTIN, 0);
+	pinMode(CLK_PIN, OUTPUT);
+	digitalWrite(CLK_PIN, 0);
 
-	Serial.print(F("Connecting to "));
-	Serial.println(ssid);
+	pinMode(DATA_PIN, OUTPUT);
+	digitalWrite(DATA_PIN, 0);
+
 	WiFi.mode(WIFI_STA);
 	WiFi.begin(ssid, password);
 	while (WiFi.status() != WL_CONNECTED) {
-		Serial.print(F("."));
-		digitalWrite(LED_BUILTIN, 1);
+		blit_solid_leds(0x10, 0x10, 0x10);
 		delay(50);
-		digitalWrite(LED_BUILTIN, 0);
+		blit_solid_leds(0, 0, 0);
 		delay(125);
 	}
-	digitalWrite(LED_BUILTIN, 1);
-	Serial.println(F(" OK"));
+	blit_solid_leds(0, 0x10, 0);
 
 	server.begin();
-	Serial.print(F("Listening at http://"));
-	Serial.println(WiFi.localIP());
 }
 
 void loop()
@@ -242,13 +303,8 @@ void loop()
 
 	enum request_type req_type = REQUEST_UNDEFINED;
 
-	digitalWrite(LED_BUILTIN, 0);
-	Serial.println(F("client connected"));
-
 	// Read the first line of the request
 	String request = client.readStringUntil('\r');
-	Serial.println(F("request: "));
-	Serial.println(request);
 
 	if (request.startsWith("GET / ")) {
 		req_type = REQUEST_INDEX;
@@ -264,35 +320,24 @@ void loop()
 	while (client.available())
 		client.read();
 
-
 	switch (req_type) {
 
 	case REQUEST_INDEX:
-		Serial.print(F("client got index"));
 		respond_index(client);
 		break;
 
 	case REQUEST_NEW_ANIM:
-		Serial.print(F("client is switching animation from "));
-		Serial.print(animation_names[(int) anim]);
-		Serial.print(F(" to "));
-		Serial.print(animation_names[(int) new_anim]);
-		respond_ok(client);
+		respond_new_anim(client);
 		break;
 
 	case REQUEST_UNDEFINED:
-		Serial.println(F("bad request"));
 		respond_not_found(client);
 		break;
 
 	default:
-		Serial.print(F("unimplemented request of type "));
-		Serial.println(req_type);
 		respond_not_found(client);
 		break;
 	}
 
 	// client is flushed and disconnected when it goes out of scope
-	Serial.println(F("dropping connection"));
-	digitalWrite(LED_BUILTIN, 1);
 }
